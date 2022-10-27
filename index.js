31acf36184a792b5fc7c47c4839d3ca5519e2069
Saadi-Rahman <sadi6280@gmail.com>
2022-10-27 13:42:21 +0600

    write some code to find and filter from data file

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/categories.json');
+const course = require('./data/course.json');
 
 app.get('/', (req, res) => {
     res.send('BackBenchers API Running...');
@@ -13,6 +14,23 @@ app.get('/', (req, res) => {
 
 app.get('/course-categories', (req, res) => {
     res.send(categories);
+})
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if(id === '08') {
+        res.send(course);
+    }
+    else {
+        const category_course = course.filter( c => c.category_id === id);
+        res.send(category_course);
+    }
+})
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = course.find( c => c._id === id);
+    res.send(selectedCourse);
 })
 
 app.listen(port, () => {
